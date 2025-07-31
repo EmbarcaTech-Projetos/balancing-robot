515ab14fa6ff09047cfa573e520d3769c4411df7
MarcosViniciusG <marcos.vnc.gundel.silva@gmail.com>
2025-07-31 17:44:17 -0300

    feat: removed 1C test and added 2A test

--- src/main.c
+++ src/main.c
@@ -3,90 +3,37 @@
 #include "hardware/i2c.h"
 #include "motor.h"
 #include "mpu6050_i2c.h" 
-
-// Emergency button pin
-#define BUTTON_B 6
+#include "math.h"
 
 int main() {
-    // Setup
     stdio_init_all();
-    gpio_init(BUTTON_B);
-    gpio_set_dir(BUTTON_B, GPIO_IN);
-    gpio_pull_up(BUTTON_B);
     mpu6050_setup_i2c();
     mpu6050_reset();
     motor_setup();
-    motor_enable();
+    // Here you can blink a led or print to indicate that program flashed and is running
 
-    // Variables for IMU
     int16_t accel_raw[3], gyro_raw[3], temp;
-    float accel[3], gyro[3];
-
-    uint16_t power = (uint16_t)(0.35 * 255) << 8;
-    motor_set_both_level(power, false); // false = forward
+    float accel[3];
 
     while (true) {
-
-        // Read IMU
         mpu6050_read_raw(accel_raw, gyro_raw, &temp);
 
         for (uint8_t i = 0; i < 3; ++i) {
             accel[i] = (float)accel_raw[i] / 16384.0f;
-            gyro[i] = (float)gyro_raw[i] / 131.0f;
         }
 
-        // Check emergency stop by sensor high values or button
-        bool high_accel = (accel[2] > 0.450f) || (accel[2] < -0.450f);
-        bool high_gyro = (gyro[1] > 420.0f) || (gyro[1] < -420.0f);
-        bool emergency_button = !gpio_get(BUTTON_B); // Active low
-        sleep_ms(25); // let microcontroller rest between readings
 
-        if (high_accel || high_gyro || emergency_button) {
-            motor_set_both_level(0, true); // Stop motors with level=0
-            // motor_disable(); // the motor wont enable again automatically
-            sleep_ms(4000); // Give the user 4 seconds to solve the emergency
-            motor_set_both_level(power,false);
-        }
+        // Threshold test on pitch (example using X-axis)
+        // Adjust threshold as needed (e.g., 0.5g ~ 30º)
+        // Inside the if/else you can blink led or print, so you have a feedback
+        if (fabsf(accel[0]) > 0.5f) {
+            motor_enable();
+            bool direction_test = accel[0] < 0; // direction based on tilt
+            motor_set_both_level(100 << 8, direction_test);
+         }
+        else { motor_set_both_level(0, false); }
+
+
+        sleep_ms(100);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// int main() {
-//     stdio_init_all();
-//     motor_setup();
-//     motor_enable();
-
-//     // 50% power forward
-//     uint16_t power = (uint16_t)(0.50 * 255) << 8;
-//     motor_set_both_level(power, false); // false = forward
-
-//     sleep_ms(10000); // Run the motors for 10 seconds
-
-//     motor_set_both_level(0, false); // stop
-
-//     while (true) {
-//         tight_loop_contents(); // Keep the program running
-//     }
-// }
